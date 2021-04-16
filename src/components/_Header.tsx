1f67c31ac29a5ba27ef81f80d7332a72bf1c6c85
karim fanti <karimwebdevelopment@gmail.com>
2021-04-16 16:01:46 +0200

    added overlay when nav active

--- src/components/_Header.tsx
+++ src/components/_Header.tsx
@@ -2,30 +2,33 @@ import { useState } from 'react'
 import styles from '../styles/components/Header.module.scss'
 
 export function Header() {
-  const [isActive, setIsActive] = useState(false)
+  const [isMenuActive, setIsMenuActive] = useState(false)
 
   function showMenu() {
-    setIsActive((prevstate) => !prevstate)
+    setIsMenuActive((prevstate) => !prevstate)
   }
 
   return (
-    <header className={styles.header}>
-      <div className={styles.content}>
-        <img src="/logo.svg" alt="designo logo"/>
-        <div 
-          className={`${isActive && `${styles.mobileTogglerActive}`} ${styles.mobileToggler}`} 
-          onClick={showMenu}
-        >
-          <span className={styles.bar}></span>
-          <span className={styles.bar}></span>
-          <span className={styles.bar}></span>
+    <>
+      <div className={`${isMenuActive && `${styles.overlayActive}`} ${styles.overlay}`} />
+      <header className={styles.header}>
+        <div className={styles.content}>
+          <img src="/logo.svg" alt="designo logo"/>
+          <div 
+            className={`${isMenuActive && `${styles.mobileTogglerActive}`} ${styles.mobileToggler}`} 
+            onClick={showMenu}
+          >
+            <span className={styles.bar}></span>
+            <span className={styles.bar}></span>
+            <span className={styles.bar}></span>
+          </div>
+          <nav className={`${isMenuActive && `${styles.navigationActive}`} ${styles.navigation}`}>
+            <a href="#" className={styles.navLink}>Our Company</a>
+            <a href="#" className={styles.navLink}>Locations</a>
+            <a href="#" className={styles.navLink}>Contact</a>
+          </nav>
         </div>
-        <nav className={`${isActive && `${styles.navigationActive}`} ${styles.navigation}`}>
-          <a href="#" className={styles.navLink}>Our Company</a>
-          <a href="#" className={styles.navLink}>Locations</a>
-          <a href="#" className={styles.navLink}>Contact</a>
-        </nav>
-      </div>
-    </header>
+      </header>
+    </>
   )
 }
