481ba98879879d724b93e17796a9dac8cb7ca868
karim fanti <karimwebdevelopment@gmail.com>
2021-04-23 15:17:45 +0200

    Design Link mobile ready

--- src/components/_DesignLink.tsx
+++ src/components/_DesignLink.tsx
@@ -1,18 +1,31 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/components/DesignLink.module.scss'
 
 interface DesignLinkProps {
   title: string
   imgURL: string
+  pageURL: string
 }
 
-export const DesingLink: React.FC<DesignLinkProps> = ({ title, imgURL }) => {
+export const DesingLink: React.FC<DesignLinkProps> = (props) => {
   return (
-    <div className={styles.cardContainer}>
-      <Image
-        src={imgURL}
-        layout='fill'
-      />
-    </div>
+    <li className={styles.cardContainer}>
+      <Link href={`${props.pageURL}`}>
+        <a>
+          <Image
+            src={props.imgURL}
+            layout='fill'
+            objectFit='cover'
+            className={styles.background}
+          />
+          <h1 className="title-primary-white">{props.title}</h1>
+          <span>
+            View projects
+            <img src="/assets/shared/desktop/icon-right-arrow.svg"/>
+          </span>
+        </a>
+      </Link>
+    </li>
   )
 }
