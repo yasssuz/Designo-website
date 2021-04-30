99739b3d6542b8bf7450dc5337761fee94741991
karim fanti <karimwebdevelopment@gmail.com>
2021-04-30 15:36:34 +0200

    improved accessibility with 'lang'

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,9 @@ import Document, { Html, Head, Main, NextScript } from "next/document"
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500&display=swap" rel="stylesheet" />
         </Head>
         <body>
