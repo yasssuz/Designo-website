a271ea8f59c687863c9490761837898ddaa1d691
karim fanti <karimwebdevelopment@gmail.com>
2021-05-02 17:09:01 +0200

    responsive mobile to tablet

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -30,7 +30,7 @@ export default function About() {
           title='World-class talent'
           paragraph1='We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.'
           paragraph2='Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission.'
-          imgSrc="/assets/about/mobile/image-world-class-talent.jpg"
+          imgSrc="/assets/about/tablet/image-world-class-talent.jpg"
         />
         <section className={styles.locations}>
           <LocationsRow />
@@ -39,7 +39,7 @@ export default function About() {
           title='The real deal'
           paragraph1='As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.'
           paragraph2='We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.'
-          imgSrc="/assets/about/mobile/image-real-deal.jpg"
+          imgSrc="/assets/about/desktop/image-real-deal.jpg"
         />
       </main>
     </>
