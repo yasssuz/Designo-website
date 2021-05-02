efd6cab970e675d5488333bb9c8438f2eb2fc1f6
karim fanti <karimwebdevelopment@gmail.com>
2021-05-02 18:27:25 +0200

    full responsive

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,7 +12,7 @@ export default function About() {
         <meta name="description" content="Founded in 2010, we are a creative agency that produces lasting results for our clients. We’ve partnered with many startups, corporations, and nonprofits alike to craft designs that make real impact. We’re always looking forward to creating brands, products, and digital experiences that connect with our clients’ audiences." />
       </Head>
 
-      <main>
+      <main className={styles.mainContainer}>
         <section className={styles.banner}>
           <div>
             <Image
@@ -30,7 +30,7 @@ export default function About() {
           title='World-class talent'
           paragraph1='We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.'
           paragraph2='Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission.'
-          imgSrc="/assets/about/tablet/image-world-class-talent.jpg"
+          imgSrc="/assets/about/desktop/image-world-class-talent.jpg"
         />
         <section className={styles.locations}>
           <LocationsRow />
