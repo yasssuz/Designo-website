6311800a927f10ce2fc166056b0f45632262390d
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-19 17:45:56 +0200

    mobile only

---
+++ src/components/home/_Hero.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from '../../styles/components/home/Hero.module.scss'
+
+export function Hero() {
+  return (
+    <section className={styles.hero}>
+      <h1 className="title-primary title-primary-white">Award-winning custom designs and digital branding solutions</h1>
+      <p className="paragraph-primary">With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services.</p>
+      <a href="#" className="button-primary">Learn More</a>
+    </section>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Header } from '../components/shared/_Header'
+import { Hero } from '../components/home/_Hero'
 import styles from '../styles/views/Home.module.scss'
 
 export default function Home() {
@@ -12,25 +11,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className="padding-container">
-          <section className={styles.hero}>
-            <div className={`${styles.hero__content}`}>
-              <h1 className={`${styles.hero__title} title-primary`}>Award-winning custom designs and digital branding solutions</h1>
-              <p className={`${styles.hero__description} paragraph-primary`}>With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services.</p>
-              <a href="#" className={`${styles.hero__buttonLink} button-primary`}>Learn More</a>
-              {/* <div className={styles.hero__illutration}>
-                <Image 
-                  src="/assets/home/desktop/image-hero-phone.png"
-                  alt="phone illustration"
-                  width={ 624 }
-                  height={ 913 }
-                  layout='fixed'
-                />
-                <img src="/assets/home/desktop/image-hero-phone.png" alt="phone illustration"/>
-              </div> */}
-            </div>
-          </section>
-        </div>
+        <Hero />
       </main>
 
       <br />
