0423b7a98edebf1c220d37ada5c75176ea1f5af6
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-19 22:36:49 +0200

    mobile ready

---
+++ src/components/home/_Advantage.tsx
@@ -0,0 +1,20 @@
+import styles from '../../styles/components/home/Advantage.module.scss'
+
+interface AdvantageProps {
+  title: string
+  description: string
+}
+
+export function Advantage(props: AdvantageProps) {
+  const { title, description } = props
+
+  return (
+    <li className={styles.card}>
+      <div className={styles.illustration}>
+        <img src={`/assets/home/desktop/illustration-${title}.svg`} alt="advantage" />
+      </div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from 'next/head'
+import { Advantage } from '../components/home/_Advantage'
 import { Hero } from '../components/home/_Hero'
 import { DesingLink } from '../components/shared/_CardLink'
+import { ContactCard } from '../components/shared/_ContactCard'
 import styles from '../styles/views/Home.module.scss'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Designo | Home</title>
         <meta name="description" content="With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services." />
@@ -13,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <Hero />
-        <section className={styles.cardsLinks}>
+        <section className={styles.linksList}>
           <ul style={{ listStyle: 'none' }}>
             <DesingLink
               title="Web Design"
@@ -26,59 +28,31 @@ export default function Home() {
               imgURL="/assets/home/desktop/image-app-design.jpg"
             />
             <DesingLink
-              title="Web Design"
-              pageURL="/design/web-design"
-              imgURL="/assets/home/desktop/image-web-design-small.jpg"
+              title="Graphic Design"
+              pageURL="/design/graphic-design"
+              imgURL="/assets/home/desktop/image-graphic-design.jpg"
+            />
+          </ul>
+        </section>
+        <section className={styles.advantagesList}>
+          <ul style={{ listStyle: 'none' }}>
+            <Advantage
+              title="passionate"
+              description="Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions."
+            />
+            <Advantage
+              title="resourceful"
+              description="Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs."
+            />
+            <Advantage
+              title="friendly"
+              description=" We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide."
             />
           </ul>
         </section>
       </main>
 
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-    </div>
+      <ContactCard />
+    </>
   )
 }
