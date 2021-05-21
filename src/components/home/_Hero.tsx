15094c35f1bea5e722d20925fc771300dbdb8326
Karim Yassine <karimwebdevelopment@gmail.com>
2021-05-21 15:20:28 +0200

    deploy ready

--- src/components/home/_Hero.tsx
+++ src/components/home/_Hero.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Link from 'next/link';
 import styles from '../../styles/components/home/Hero.module.scss'
 
 export function Hero() {
@@ -6,7 +6,9 @@ export function Hero() {
     <section className={styles.hero}>
       <h1 className="title-primary title-primary-white">Award-winning custom designs and digital branding solutions</h1>
       <p className="paragraph-primary">With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services.</p>
-      <a href="#" className="button-primary">Learn More</a>
+      <Link href="/about">
+        <a href="#" className="button-primary">Learn More</a>
+      </Link>
     </section>
   )
 }
